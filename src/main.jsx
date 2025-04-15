e8081613dae020ac8f51a1ec7949b8edfac655df
rahat-shanto <rahatahmedshanto0910@gmail.com>
2025-04-15 21:38:34 +0600

    added functionality of login and register

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import Root from './Layout/Root/Root.jsx'
 import Home from './Pages/Home/Home.jsx'
 import AuthProvider from './Provider/AuthProvider.jsx'
 import ProductCart from './Pages/ProductCart/ProductCart.jsx'
-import Registration from './Pages/Registration/Registration.jsx'
-import Login from './Pages/Login/Login.jsx'
+// import Registration from './Pages/Registration/Registration.jsx'
+// import Login from './Pages/Login/Login.jsx'
 import PrivateRoutes from './Routes/PrivateRoutes.jsx'
+import Login from './Pages/Login/Login.jsx'
+import Register from './Pages/Registration/Register.jsx'
 
 // routing section
 const router = createBrowserRouter([
@@ -29,8 +31,8 @@ const router = createBrowserRouter([
         element:<Login></Login>
       },
       {
-        path: '/registration',
-        element:<Registration></Registration>
+        path: '/register',
+        element:<Register></Register>
       }
       
     ]
